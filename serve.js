d567306ab3c2622713e3e606138ca3eb718bf244
choephix <i@thechoephix.com>
2019-10-17 00:58:20 +0000

    ini

---
+++ serve.js
@@ -0,0 +1,20 @@
+console.clear()
+const express = require("express")
+const app = express()
+const PORT = parseInt( process.argv[2] || 4333 )
+
+const sleep = ms => new Promise( done => setTimeout( done, ms ) )
+
+
+app.use( express.static( 'www' ) )
+app.use( express.json( { extended: true } ) )
+app.post('/backend/api/start', (req, res) => {
+  console.log(req.url,req.body,'\n')
+  return responses.current( req, res )
+})
+
+server = app.listen( PORT, () => onServerStarted() )
+
+function onServerStarted() {
+  console.log("\x1b[94m» Server started at port \x1b[93;1m%s\x1b[0m", server.address().port) 
+}
