ea40c3fb4a5c0b7b9a36a78533cc4d84421fcccc
stefan.ginev <stefan.ginev@blockchain-solutions.co>
2019-10-23 22:30:28 +0300

    typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/jstree/index.d.ts" />
 /// <reference path="globals/three/index.d.ts" />
+/// <reference path="modules/firebase/index.d.ts" />
 /// <reference path="modules/http-proxy-agent/index.d.ts" />
 /// <reference path="modules/https-proxy-agent/index.d.ts" />
